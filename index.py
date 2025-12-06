9424cfcd2852250241a1dc7664c20fe1ebd8f115
lugewxp <166201789+lugewxp@users.noreply.github.com>
2025-12-06 12:27:31 +0800

    提交了个基本功能😁

--- index.py
+++ index.py
@@ -1,1 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
 
+url = input("请输入网页URL: ")
+try:
+response = requests.get(url)
+response.raise_for_status()
+response.encoding = 'utf-8'
+soup = BeautifulSoup(response.text, 'html.parser')
+text = soup.get_text()
+print(text)
+except requests.exceptions.RequestException as e:
+print("错误: ", e)
